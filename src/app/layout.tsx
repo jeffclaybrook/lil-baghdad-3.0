1fb523732c1aa1815ae71e4251add92c6f39f6b0
Jeff Claybrook <jeffclaybrook@Jeffs-MBP.home>
2024-07-14 14:02:48 -0500

    Added Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,27 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import { ReactNode } from "react"
+import { GoogleAnalytics } from "@next/third-parties/google"
+import "./globals.css"
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Lil Baghdad",
-  description: "Iraqi made, Texas inspired",
+  description: "Iraqi made, Texas inspired"
 };
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: ReactNode
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <GoogleAnalytics gaId="G-JDJYK3LM1M" />
+        {children}
+      </body>
     </html>
   );
 }
